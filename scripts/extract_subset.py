a40df1c9b61e4978b728c5c3a9e27142fffed70f
elias <eliasls2002@yahoo.de>
2025-11-15 12:57:58 +0100

    again

--- scripts/extract_subset.py
+++ scripts/extract_subset.py
@@ -15,14 +15,14 @@ print(train.head())
 print(test.head())
 
 # Choose how many rows you want to sample
-subset_size = 100   # <-- change this as you like
+subset_size = 2000   # <-- change this as you like
 
 # Create subsets (random but reproducible)
 train_subset = train.sample(n=subset_size, random_state=SEED)
 test_subset = test.sample(n=subset_size, random_state=SEED)
 
 # Save them
-train_subset.to_csv("data/train_subset.csv", index=False)
-test_subset.to_csv("data/test_subset.csv", index=False)
+train_subset.to_csv("data/train_subset.csv", index=False, delimiter=';')
+test_subset.to_csv("data/test_subset.csv", index=False, delimiter=';')
 
 print("Subset files saved: train_subset.csv, test_subset.csv")
